6f58eceefbf346cad0823c419a5a36a3058b08ff
fajsjv <FAJSJ@vestas.com>
2024-12-27 16:16:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        if not search_on: 
+            search_on = fruit_chosen
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         # st.text(smoothiefroot_response.json())
